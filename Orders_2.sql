1cce60558868863c91801f46d1de403a128db4b1
Mouparna Biswas <85263019+Moupython@users.noreply.github.com>
2025-12-02 00:41:36 -0800

    Update Orders_2.sql

--- Orders_2.sql
+++ Orders_2.sql
@@ -1,14 +1,19 @@
 use restaurant ; 
 ---------- What is the date range of the table -----------------
-with t1 as (select max(order_date)  as Highest_Date, min (Order_date) as Minimum_date  
+with t1 as (select max(order_date)  as Highest_Date, min (Order_date) as Minimum_date  ---- using CTE Table 
 from 
 dbo.order_details)
-
+  
+----------- How many orders were made within this date range ---------------
+------------How many items were made within this date range ----------------  
+  
 Select count(Order_id) as Order_count , count(item_id) as Item_Count 
 from 
 Restaurant.dbo.order_details
 inner join t1
 on order_date between t1.minimum_date and t1.highest_date ; 
+
+-------------Which Order had most number of Items ----------------------
 
 with Ord as (select order_id , 
 count(item_id) cnt, 
@@ -19,10 +24,11 @@ group by Order_id)
 Select Order_id , ord.cnt
 from Ord 
 where rn = 1; 
-
+----------------How many Orders had more than 12 items ------------------
 select order_id 
 from Restaurant.dbo.order_details
 group by order_id 
 having count(Item_id) > 12; 
+
 
 
